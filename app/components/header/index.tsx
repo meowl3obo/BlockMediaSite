4b0ede4e3b67d04078138f813cbf676044ca5dad
meowl3obo <dear91304526@gmail.com>
2023-07-02 17:44:16 +0800

    first commit

---
+++ app/components/header/index.tsx
@@ -0,0 +1,11 @@
+import "./style.scss";
+
+export default () => {
+  return (
+    <header className="header">
+      <div id="start">MeowlTube</div>
+      <div id="center">Search</div>
+      <div id="end">User</div>
+    </header>
+  );
+};
