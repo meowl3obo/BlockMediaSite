54f3db469594e0d5ca59bb5aa15d7895d0598b2a
meowl3obo <dear91304526@gmail.com>
2023-07-05 16:18:36 +0800

    channel metadata

--- app/channel/[id]/page.tsx
+++ app/channel/[id]/page.tsx
@@ -1,5 +1,9 @@
 import { IParams } from '@/app/types/route'
 
+export const metadata = {
+  title: "頻道內容 - MeowlTube",
+};
+
 export default ({ params }: IParams) => {
   const id = params['id']
   return (
